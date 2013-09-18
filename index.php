23152d16563661107c3feecb6ca72e35beff15c8
Carolyn Kelley <carolynkelley311@gmail.com>
2013-09-17 23:55:51 -0400

    updates to index.php, added css file

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Carolyn Kelley | p1 | dwa15 | Fall 2013</title>
+	<link rel="stylesheet" type="text/css" href="p1-ck311.css">
+</head>
+
+<body>
+<h1>Carolyn Kelley</h1>
+<h2>Online Communications Specialist / Harvard Law School Office of Communications</h2>
+<h3>"So what, exactly, do you do?"</h3>
+<p>I work in the Harvard Law School Office of Communications as the primary news content editor and CMS sufferer. Our team has recently begun work on a new project which will have us building a news site in WordPress, so I am taking the opportunity to strengthen my dev skills to become as involved in as possible--or, at least, conversant--in all aspects of the project.</p>
+
+<p>When I'm not busy applying inline styles, I <a href="http://www.choruspromusica.org">sing</a> a lot, <a href="http://www.mikesfitnessjp.com">jog slowly</a>, and continue my quest to find the <a href="http://https://www.facebook.com/pages/Ula-Cafe/63364355029">best coffee shop</a> in Boston. </p>
+</body>
+</html>
