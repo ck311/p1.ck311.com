1224795dd06cde5de122e7297fef10fc51ea601f
ck311 <ck311@users.noreply.github.com>
2013-09-18 06:50:45 -0400

    Update index.php

--- index.php
+++ index.php
@@ -8,9 +8,9 @@
 
 <body>
 <h1>Carolyn Kelley</h1>
-<h2>Online Communications Specialist / Harvard Law School Office of Communications</h2>
+<h2>Online Communications Specialist</h2>
 <h3>"So what, exactly, do you do?"</h3>
-<p>I work in the Harvard Law School Office of Communications as the primary news content editor and CMS sufferer. Our team has recently begun work on a new project which will have us building a news site in WordPress, so I am taking the opportunity to strengthen my dev skills to become as involved in as possible--or, at least, conversant--in all aspects of the project.</p>
+<p>I work in the Harvard Law School Office of Communications as the primary news content editor. Our team has recently begun work on a new project which involves WordPress, so I am taking the opportunity to strengthen my dev skills so I can become as involved in as possible--or, at least, conversant--in all aspects of it.</p>
 
 <p>When I'm not busy applying inline styles, I <a href="http://www.choruspromusica.org">sing</a> a lot, <a href="http://www.mikesfitnessjp.com">jog slowly</a>, and continue my quest to find the <a href="http://www.facebook.com/pages/Ula-Cafe/63364355029">best coffee shop</a> in Boston. </p>
 </body>
