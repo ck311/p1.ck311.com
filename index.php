848e1886839d7fc45da538ac9f1768cdfcb1a670
Carolyn Kelley <carolynkelley311@gmail.com>
2013-09-18 00:39:33 -0400

    fixed broken link

--- index.php
+++ index.php
@@ -12,6 +12,6 @@
 <h3>"So what, exactly, do you do?"</h3>
 <p>I work in the Harvard Law School Office of Communications as the primary news content editor and CMS sufferer. Our team has recently begun work on a new project which will have us building a news site in WordPress, so I am taking the opportunity to strengthen my dev skills to become as involved in as possible--or, at least, conversant--in all aspects of the project.</p>
 
-<p>When I'm not busy applying inline styles, I <a href="http://www.choruspromusica.org">sing</a> a lot, <a href="http://www.mikesfitnessjp.com">jog slowly</a>, and continue my quest to find the <a href="http://https://www.facebook.com/pages/Ula-Cafe/63364355029">best coffee shop</a> in Boston. </p>
+<p>When I'm not busy applying inline styles, I <a href="http://www.choruspromusica.org">sing</a> a lot, <a href="http://www.mikesfitnessjp.com">jog slowly</a>, and continue my quest to find the <a href="http://www.facebook.com/pages/Ula-Cafe/63364355029">best coffee shop</a> in Boston. </p>
 </body>
 </html>
